1b96cf379564725d0a6e74d44852777932a33d70
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2024-03-18 19:07:07 -0300

    #6 criando as configurações do do sequelize

---
+++ config/sequelizeCli.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    development:{
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    }
+};
---
+++ src/database/sequelize.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export abstract class Database {
+    protected sequelize = new Sequelize({
+        dialect: "mysql",
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        define: {
+            underscored: true
+        }
+    })
+};

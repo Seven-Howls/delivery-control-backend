d0c3cf7b23c08c2d6d94cf5f25751dd5a982d5ab
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2024-04-27 03:20:56 -0300

    Criando o TODO do controller de colaborador

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,28 +2,29 @@ import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
 
 export class UserController {
-  private userBusiness: UserBusiness
-  constructor(userBusiness: UserBusiness) {
-    this.userBusiness = userBusiness;
-  }
-
-  /* signupCollaborator = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { name, email, password } = req.body;
-      const token = await this.userBusiness.signup({ name, email, password });
-      res.status(200).send({ token });
-    } catch (error: any) {
-      res.status(error.statusCode || 400).send({ error: error.message });
+    private userBusiness: UserBusiness
+    constructor(userBusiness: UserBusiness) {
+        this.userBusiness = userBusiness;
     }
-  }; */
 
-  login = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { cpf, password } = req.body;
-      const token = await this.userBusiness.login({ cpf, password });
-      res.status(200).json(token).send();
-    } catch (error: any) {
-      res.status(error.statusCode || 400).send({ error: error.message });
-    }
-  };
+    signupCollaborator = async (req: Request, res: Response): Promise<void> => {
+        try {
+            //TODO : Pegar o token
+            //TODO : Pegar os dados de usuario empresa e tipo do body
+            //TODO : Chamar a funcao do business
+            //TODO : Retorna somente uma mensagem dizendoq eu usuario foi criado
+        } catch (error: any) {
+            res.status(error.statusCode || 400).send({ error: error.message });
+        }
+    };
+
+    login = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const { cpf, password } = req.body;
+            const token = await this.userBusiness.login({ cpf, password });
+            res.status(200).json(token).send();
+        } catch (error: any) {
+            res.status(error.statusCode || 400).send({ error: error.message });
+        }
+    };
 }

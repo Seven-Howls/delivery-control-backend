5b5a63fda4e43f41b66dd1b541920431e7ed3342
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2024-05-06 23:47:33 -0300

    Implementando a roda de cadastro de entrega

--- src/router/deliveriesRouter.ts
+++ src/router/deliveriesRouter.ts
@@ -4,16 +4,26 @@ import { DeliveriesBusiness } from "../business/DeliveriesBusiness";
 import { DeliveriesData } from "../data/DeliveriesData";
 import { MotoboyData } from "../data/MotoboyData";
 import { StatusData } from "../data/StatusData";
+import { PaymentMethodData } from "../data/PaymentMethodData";
+import { UserTypePermissionsData } from "../data/UserTypePermissionsData";
+import { CollaboratorData } from "../data/CollaboratorData";
+import { DeliveryFeeData } from "../data/DeliveryFeeData";
 
 export const deliveriesRouter = express.Router();
 const deliveriesData = new DeliveriesData();
 const motoboyData = new MotoboyData();
 const statusData = new StatusData();
+const deliveryFeeData = new DeliveryFeeData();
+const paymentMethodData = new PaymentMethodData();
+const userTypePermissionsData = new UserTypePermissionsData();
+const collaboratorData = new CollaboratorData();
 
-const deliveriesBusiness = new DeliveriesBusiness(deliveriesData,motoboyData,statusData);
+const deliveriesBusiness = new DeliveriesBusiness(deliveriesData,motoboyData,statusData,deliveryFeeData,paymentMethodData,userTypePermissionsData,collaboratorData);
 
 const deliveriesController = new DeliveriesController(deliveriesBusiness);
 
 deliveriesRouter.get("/in-progress/:motoboyId", deliveriesController.getDeliveriessInProgressByMotoBoy);
 deliveriesRouter.get("/history/:motoboyId", deliveriesController.getHistoryDeliveriesByMotoboy);
-deliveriesRouter.put("/:id", deliveriesController.updateDeliveryStatusById)
+deliveriesRouter.put("/:id", deliveriesController.updateDeliveryStatusById);
+deliveriesRouter.post("/create", deliveriesController.createDelivery);
+

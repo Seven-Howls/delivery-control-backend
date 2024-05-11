1b6bc0080e0520fac88ebd0059decaa0e6695be1
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2024-05-11 02:28:57 -0300

    Editando o init.sh

--- init.sh
+++ init.sh
@@ -1,43 +1,15 @@
-echo "Valor de $1"
-echo "User mysql"
-read MYSQL_USER
-echo "Password mysql"
-read MYSQL_PASSWORD
-
-# Comando para criar um novo banco de dados
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE SCHEMA \`delivery-control\`;"
-
-echo "\n\n##Banco de dados 'delivery-control' criado com sucesso.##"
-
-cat > '.env' <<EOT
-DB_HOST=localhost
-DB_USER=root
-DB_PASSWORD=$MYSQL_PASSWORD
-DB_NAME=delivery-control
-DB_PORT=3306
-DB_DIALECT=mysql
-API_PORT=8080
-BCRYPT_COST=10
-BCRYPT_KEY=12345
-JWT_KEY=12345
-ENVIRONMENT_PROD=FALSE
-EOT
-
-echo "\n\n##.env criado com sucesso##"
-
-npm install
-
-echo "\n\n##Dependencias instaladas com sucesso##"
-
-npx sequelize-cli db:migrate
-
-echo "\n\nMigrações executadas com sucesso"
-
-if [ "$1" = "limpa" ]; then
-  echo "\n\nA base subiu limpa"
-  
-else
-    npx sequelize-cli db:seed:all
-    echo "\n\nTodas as seeds foram executadas"
-  
-fi
+#Funções padrão para a instalação do CRM
+for arg in "$@"; do
+    case $arg in
+        "--migrate" )
+            npx sequelize-cli db:migrate;;
+        "--npm" )
+            npm install --unsafe-perm --legacy-peer-deps && npm build;;
+        "--seed" )
+            npx sequelize-cli db:seed:all;; 
+        "--full" )
+            npm install --unsafe-perm --legacy-peer-deps && npm build &&\
+            npx sequelize-cli db:migrate &&\
+            npx sequelize-cli db:seed:all 
+    esac
+done

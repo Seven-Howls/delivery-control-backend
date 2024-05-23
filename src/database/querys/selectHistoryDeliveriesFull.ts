4ddc94882188cbb3e18b4a9d1722b9af802afc54
sergiolfaria <srgion@yahoo.com.br>
2024-05-23 16:05:43 -0300

    correção do select history delivery

--- src/database/querys/selectHistoryDeliveriesFull.ts
+++ src/database/querys/selectHistoryDeliveriesFull.ts
@@ -6,10 +6,10 @@ SELECT
     JSON_ARRAYAGG(
         JSON_OBJECT(
             'id', e.id,
-            'taxa_entrega', (SELECT JSON_OBJECT('id', te.id, 'descricao', te.descricao) FROM taxa_entrega AS te WHERE te.id = e.taxa_entrega_id),
-            'motoboy', (SELECT JSON_OBJECT('id', m.id, 'nome', m.nome) FROM motoboy AS m WHERE m.id = e.motoboy_id),
-            'metodo_pagamento', (SELECT JSON_OBJECT('id', mp.id, 'descricao', mp.descricao) FROM metodo_pagamento AS mp WHERE mp.id = e.metodo_pagamento_id),
-            'status', (SELECT JSON_OBJECT('id', s.id, 'descricao', s.descricao) FROM status AS s WHERE s.id = e.status_id),
+            'taxas_entregas', (SELECT JSON_OBJECT('id', te.id, 'descricao', te.descricao) FROM taxa_entregas AS te WHERE te.id = e.taxa_entrega_id),
+            'motoboy', JSON_OBJECT('id', m.id, 'nome', u.nome),
+            'metodos_pagamentos', (SELECT JSON_OBJECT('id', mp.id, 'nome', mp.nome) FROM metodos_pagamentos AS mp WHERE mp.id = e.metodo_pagamento_id),
+            'status', (SELECT JSON_OBJECT('id', s.id, 'nome', s.nome) FROM status AS s WHERE s.id = e.status_id),
             'valor_produto', e.valor_produto,
             'taxa_servico', e.taxa_servico,
             'valor_liquido', e.valor_liquido,
@@ -20,7 +20,9 @@ SELECT
         )
     ) AS entregas_dados
 FROM entregas AS e
+JOIN motoboys AS m ON e.motoboy_id = m.id
+JOIN usuarios AS u ON m.usuario_id = u.id
 WHERE e.motoboy_id = :motoboyId
 AND e.deleted_at IS NULL
 GROUP BY data_entrega;
-`
+`;

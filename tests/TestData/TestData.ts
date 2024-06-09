ddb6618ae0af7ddb61dafa410d2d64f382d59c7b
sergiolfaria <srgion@yahoo.com.br>
2024-06-09 20:50:44 -0300

    Singup colaborador feito

--- tests/TestData/TestData.ts
+++ tests/TestData/TestData.ts
@@ -10,3 +10,29 @@ export const updatedData= {
     nome: "Thenmarques",
     email: "novoemail2@exemplo.com",
 };
+function generateRandomEmail() {
+    const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let string = '';
+    for(let ii=0; ii<15; ii++){
+        string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return string + "@gmail.com";
+}
+
+function generateRandomCPF() {
+    let cpf = '';
+    for(let ii=0; ii<11; ii++){
+        cpf += Math.floor(Math.random() * 10); 
+    }
+    return cpf;
+}
+
+export const dataUserSingUp = {
+    nome: "Test Jest",
+    cpf: generateRandomCPF(),
+    password: "12345",
+    celular: 32934280255,
+    email: generateRandomEmail(),
+    typeId: "61c8de4a-d05b-4c54-b18f-1f685cb6a41f"
+};
+;
